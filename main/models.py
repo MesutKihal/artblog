9de71725d889d2ae9262eee2a546ab01aa260f82
Mesut Kihal <MesutKihal@messaoudkihal@protonmail.com>
2025-01-02 12:45:56 +0000

    ready for first deployment

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=300)
+    class Meta:
+        verbose_name_plural = "Categories"
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    description = models.TextField()
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
